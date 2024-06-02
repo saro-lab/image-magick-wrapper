704082c237bbe237f8f00ceae4c341950c93a7a0
PARK Yong Seo <j@saro.me>
2024-06-02 22:56:57 +0900

    init 3

--- src/main/kotlin/me/saro/imw/ImageMagick.kt
+++ src/main/kotlin/me/saro/imw/ImageMagick.kt
@@ -2,20 +2,31 @@ package me.saro.imw
 
 import me.saro.imw.comm.ImageMagickException
 import me.saro.imw.comm.ImageMagickResult
+import java.io.File
 
-class ImageMagick {
+class ImageMagick private constructor(
+    private val binPath: String,
+    private val input: File,
+) {
     companion object {
         @JvmStatic
-        fun execute(binPath: String, options: List<String>): ImageMagickResult {
+        fun create(binPath: String, input: File): ImageMagick {
+            return ImageMagick(binPath, input)
+        }
+
+        @JvmStatic
+        fun create(input: File): ImageMagick = create("magick", input)
+
+        @JvmStatic
+        fun manual(binPath: String, options: List<String>): ImageMagickResult =
             try {
                 val process = ProcessBuilder(binPath, *options.toTypedArray())
                     .redirectErrorStream(true)
                     .start()
                 val message = process.inputStream.use { String(it.readAllBytes()) }.trim()
-                return ImageMagickResult(process.waitFor(), message)
+                ImageMagickResult(process.waitFor(), message)
             } catch (e: Exception) {
                 throw ImageMagickException(e)
             }
-        }
     }
 }
